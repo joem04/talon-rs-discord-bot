5a4373f86cda2895f5d6fa761926261f3083a07b
jm <30784663+jm04x@users.noreply.github.com>
2024-10-07 23:37:53 +0100

    Fixed imports again

--- cogs/database.py
+++ cogs/database.py
@@ -1,5 +1,5 @@
 import discord
-from discord import app_commands
+from discord.ext import commands
 import aiosqlite
 
 class DatabaseCog(commands.Cog):
--- cogs/general.py
+++ cogs/general.py
@@ -1,5 +1,6 @@
 import discord
 from discord import app_commands
+from discord.ext import commands
 from .database import DatabaseCog  # Import the DatabaseCog for database access
 
 class GeneralCog(commands.Cog):
